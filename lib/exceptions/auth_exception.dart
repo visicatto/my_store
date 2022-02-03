91483a218e663b3c7fe2fa1a3aca9f33d36833ae
Wesley Visicatto <wesleyvisicatto@live.com>
2022-02-03 11:07:09 -0300

    Update auth_exception.dart

--- lib/exceptions/auth_exception.dart
+++ lib/exceptions/auth_exception.dart
@@ -1,7 +1,8 @@
 class AuthException implements Exception {
   static const Map<String, String> errors = {
     'EMAIL_EXISTS': 'E-mail already registered.',
-    'INVALID_PASSWORD': 'The password you entered is wrong.',
+    'INVALID_PASSWORD':
+        'The account name or password that you have entered is incorrect.',
     'USER_DISABLED': 'User disabled.',
     'OPERATION_NOT_ALLOWED': 'Operation not allowed.',
     'TOO_MANY_ATTEMPTS_TRY_LATER': 'Access temporarily blocked. Try later.',
